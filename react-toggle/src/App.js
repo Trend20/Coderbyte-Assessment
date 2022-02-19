5883b1fd42cafb9114d5ecb1a4207b8d48187b78
DevEnock <enockomondi305@gmail.com>
2022-02-19 22:02:50 +0300

    adding toggle button

--- react-toggle/src/App.js
+++ react-toggle/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      isButtonOn: false
+    }
+  }
+
+  handleButtonClick = () =>{
+    this.setState({
+      isButtonOn: !this.state.isButtonOn
+    })
+  }
+
+  render() {
+    const { isButtonOn } = this.state;
+    return (
+      <div className='app'>
+        <button onClick={this.handleButtonClick}>{ isButtonOn ? 'ON' : 'OFF'}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
