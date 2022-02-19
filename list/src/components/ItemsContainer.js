5c4df31a46a05853f0846c4fa726d4d35fc0de30
DevEnock <enockomondi305@gmail.com>
2022-02-20 00:24:41 +0300

    adding components

---
+++ list/src/components/ItemsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemsContainer extends Component {
+  render() {
+    return (
+      <div>
+        Items Container
+      </div>
+    );
+  }
+}
+
+export default ItemsContainer;
---
+++ list/src/components/ListItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListItems extends Component {
+  render() {
+    return (
+      <div>
+        List Items
+      </div>
+    );
+  }
+}
+
+export default ListItems;
