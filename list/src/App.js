4ca9ca1bbc4f192c34c3a5bc611a152f93146c66
DevEnock <enockomondi305@gmail.com>
2022-02-21 14:44:26 +0300

    updating the list project

--- list/src/App.js
+++ list/src/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import ListItems from './components/ListItems';
-import './App.css';
+import ItemsContainer from './components/ItemsContainer';
 
-class App extends Component() {
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      todoList: [],
+      item: ''
+    }
+  }
+  handleInput = (event) =>{
+    this.setState({
+      item: event.target.value
+    })
+    // console.log(event.target.value);
+  }
+
+  onFormSubmit = (event) =>{
+    event.preventDefault();
+
+    const newTodo = {
+      item: this.state.item,
+      id: Date.now()
+    }
+
+    this.setState(state =>({
+      todoList: state.todoList.concat(newTodo),
+      item: ''
+    }))
+  }
+
   render(){
     return (
-      <div className="app">
-        
-        <ListItems />
+     <div className='todo'>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="text" value={this.state.item} onChange={this.handleInput} placeholder='enter text here!'/>
+          <button type='submit' onClick={this.onFormSubmit}>Add</button>
+        </form>
+        <ItemsContainer todoList={this.state.todoList}/>
       </div>
     );
   }
--- list/src/components/ItemsContainer.js
+++ list/src/components/ItemsContainer.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 
 class ItemsContainer extends Component {
-  constructor(props){
-    super(props)
-    this.state ={
-      value: ''
-    }
-  }
-
-  
   render() {
     return (
       <div className='items-container'>
         <ul>
-          <li>{this.state.value}</li>
+          {
+            this.props.todoList.map(todoItem =>(
+              <li key={todoItem.id}>
+                {todoItem.item}
+              </li>
+            ))
+          }
         </ul>
       </div>
     );
